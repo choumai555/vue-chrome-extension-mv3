f5811b8e1589c0cb92ba954d15fe6a5ba6e86fe3
linlb <909559682@qq.com>
2022-10-28 18:11:34 +0800

    test: 💍 fix
    BREAKING CHANGE: 🧨 1

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+// GlobalComponents for Volar
+declare module 'vue' {
+  export interface GlobalComponents {
+
+  }
+}
+
+export { }
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,24 +1,24 @@
 
 export function useChrome(funName: string, ...pramas: any) {
-    let callback = function () { }
-    let callbackindex = pramas.findIndex((item: any) => typeof item === 'function')
-    if (callbackindex != -1) {
-        callback = pramas[callbackindex]
-        pramas[callbackindex] = { callback: true }
-    }
-    chrome.runtime.sendMessage({
-        funType: 'chrome',
-        funName: funName,
-        pramas,
-        //@ts-ignore
-    }, (pramas) => callback(...pramas));
+  let callback = function () { }
+  let callbackindex = pramas.findIndex((item: any) => typeof item === 'function')
+  if (callbackindex != -1) {
+    callback = pramas[callbackindex]
+    pramas[callbackindex] = { callback: true }
+  }
+  chrome.runtime.sendMessage({
+    funType: 'chrome',
+    funName: funName,
+    pramas,
+    //@ts-ignore
+  }, (pramas) => callback(...pramas));
 }
 export function axios(funName: string, pramas: any, callback: (arg0: any) => any, headers = {}) {
-    chrome.runtime.sendMessage({
-        funType: 'axios',
-        funName,
-        pramas,
-        headers
-        //@ts-ignore
-    }, (pramas) => callback(...pramas));
+  chrome.runtime.sendMessage({
+    funType: 'axios',
+    funName,
+    pramas,
+    headers
+    //@ts-ignore
+  }, (pramas) => callback(...pramas));
 }
